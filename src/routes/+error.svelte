b36a768c440aa10b1de0df72cc971f6c72ba4377
BiroNora <52859737+BiroNora@users.noreply.github.com>
2024-04-24 17:51:27 +0200

    Error page added

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="pt-24 mx-auto text-center justify-between font-poppins font-medium text-gray-400">
+	<p class="font-extrabold">{$page.status}</p>
+	<div class="pt-8">
+		<p>Sorry, looks like something went wrong.</p>
+		<div class="pt-5 font-extrabold text-xl text-red-500">
+			<a href="/">Click here to return to the Home Page</a>
+		</div>
+	</div>
+</div>
